8b5a18f3ae44418c3bb295101f66f51f09edbac4
reggie <reggie.dai@anker.com>
2025-01-22 15:22:50 +0800

    Add WhisperModel transcription script

--- django-demo/users/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class UserProfile(AbstractUser):
-    bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)
-
-    def __str__(self):
-        return self.username
